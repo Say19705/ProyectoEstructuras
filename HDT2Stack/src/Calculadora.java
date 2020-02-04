1a0ba336bd0dd281388c1f62ed2ee782c1d35665
say19705 <say19705@uvg.edu.gt>
2020-02-04 16:48:15 -0600

    Terminé la clase caluladora

--- HDT2Stack/src/Calculadora.java
+++ HDT2Stack/src/Calculadora.java
@@ -3,26 +3,26 @@ public class Calculadora implements iCalculadora {
 
 	
 	public int sumar(int operando1, int operando2) {
-		
-		return 0;
+		int result = operando1 + operando2;
+		return result;
 	}
 
 	
 	public int restar(int operando1, int operando2) {
-		
-		return 0;
+		int result = operando1 - operando2;
+		return result;
 	}
 
 	
 	public int dividir(int operando1, int operando2) {
-		
-		return 0;
+		int result = (operando1/operando2);
+		return result;
 	}
 
 	
 	public int multiplicar(int operando1, int operando2) {
-		
-		return 0;
+		int result = operando1*operando2
+		return result;
 	}
 
 }
