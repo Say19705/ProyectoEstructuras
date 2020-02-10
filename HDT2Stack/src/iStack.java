f797d9d2503bb718643917e483cc6d7acc27ea1f
say19705 <say19705@uvg.edu.gt>
2020-02-10 11:37:10 -0600

    Hoja 3

--- HDT2Stack/src/iStack.java
+++ HDT2Stack/src/iStack.java
@@ -5,26 +5,10 @@
  */
 public interface iStack<E>{
 	
-	/**
-	 * @return Revisa si el stack esta vacio
-	 */
 	public boolean empty();
-	
 	public E peek();
-	
-	/**
-	 * @param elemento
-	 * @return el elemento mas reciente en el stack
-	 */
 	public E pop();
-	
-	
-	/**
-	 * @param elemento
-	 * @return
-	 */
 	public E push(E elemento);
-	
 	public int search(E elemento);
 	
 	
