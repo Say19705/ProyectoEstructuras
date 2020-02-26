173fbeeb9a4c9338798495d5c4e4ac310dc6a4a3
say19705 <say19705@uvg.edu.gt>
2020-02-26 16:56:17 -0600

    termine el abstract

---
+++ HDT4/src/abstractList.java
@@ -0,0 +1,14 @@
+
+public abstract class abstractList<E> implements List<E>{
+	public abstractList() {
+		
+	}
+	public boolean contains(E value) {
+		return -1 != indexOf(value);
+	}
+	 public boolean isEmpty()
+	   // post: returns true iff list has no elements
+	   {
+	      return size() == 0;
+	   }
+}
